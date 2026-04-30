2dbd145c01379745599e7e125f64db66146ea63a
shyweeds <2149934895@qq.com>
2026-04-30 17:30:37 +0800

    update:neovim can <alt>+h/j/k/l to adjust the windows

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -46,6 +46,12 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Adjust the split's rates more effectively
+vim.keymap.set("n", "<A-h>", ":vertical resize +1<CR>", { desc = "Move window left" })
+vim.keymap.set("n", "<A-l>", ":vertical resize -1<CR>", { desc = "Move window right" })
+vim.keymap.set("n", "<A-j>", ":resize -1<CR>", { desc = "Move window lower" })
+vim.keymap.set("n", "<A-k>", ":resize +1<CR>", { desc = "Move window upper" })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
