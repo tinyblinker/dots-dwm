8e3b55639a90a8dba9b4cf41480f37f3e819251a
shyweeds <2149934895@qq.com>
2026-06-07 16:22:46 +0800

    add nix-config

---
+++ nix-config/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "globalTools";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    packages.${system}.default = pkgs.buildEnv {
+      name = "shyweedsTools";
+
+      paths = with pkgs; [
+        cmake
+        nodejs
+      ];
+    };
+  };
+}
