bbe99f905cdf6ff5de58fdad52cbb831f68f636c
shyweeds <2149934895@qq.com>
2026-06-17 17:20:02 +0800

    remove copilot

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,82 +1,56 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		opts = {},
-		config = function()
-			require("copilot").setup({
-				suggestion = { enabled = false },
-				panel = { enabled = false },
-			})
-		end,
-	},
-	{
-		"giuxtaposition/blink-cmp-copilot",
-	},
-	{
-		"saghen/blink.cmp",
-		event = "VimEnter",
-		version = "1.*",
-		-- optional: provides snippets for the snippet source
-		dependencies = {
-			-- copilot engine
-			"giuxtaposition/blink-cmp-copilot",
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
+	-- optional: provides snippets for the snippet source
+	dependencies = {
 
-			-- Snippet Engine
-			{
-				"L3MON4D3/LuaSnip",
-				version = "2.*",
-				build = (function()
-					-- 排除win32环境
-					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-						return
-					end
-					return "make install_jsregexp"
-				end)(),
-				dependencies = {
-					{
-						"rafamadriz/friendly-snippets",
-						config = function()
-							require("luasnip.loaders.from_vscode").lazy_load()
-						end,
-					},
-				},
-				opts = {},
-			},
-		},
-
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			keymap = { preset = "default" },
-
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-
-			-- 显示文档
-			completion = {
-				documentation = { auto_show = false, auto_show_delay_ms = 500 },
-			},
-
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer", "copilot" },
-				providers = {
-					copilot = {
-						name = "copilot",
-						module = "blink-cmp-copilot",
-						score_offset = 100,
-						async = true,
-					},
+		-- Snippet Engine
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				-- 排除win32环境
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
 				},
 			},
-			snippets = { preset = "luasnip" },
-
-			fuzzy = { implementation = "prefer_rust_with_warning" },
-
-			signature = { enabled = true },
+			opts = {},
 		},
-		opts_extend = { "sources.default" },
 	},
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = { preset = "default" },
+
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+
+		-- 显示文档
+		completion = {
+			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		},
+
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+	},
+	snippets = { preset = "luasnip" },
+
+	fuzzy = { implementation = "prefer_rust_with_warning" },
+
+	signature = { enabled = true },
+
+	opts_extend = { "sources.default" },
 }
