cb4b13cf7735985032c37376fed0c008c05e5d1e
shyweeds <2149934895@qq.com>
2026-06-26 16:25:16 +0800

    format the flake.nix

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -1,31 +1,29 @@
 {
   description = "globalTools";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "shyweedsTools";
+        paths = with pkgs; [
+          shellharden
+          ruff
+          nixfmt
+          nixd
+          black
+          stylua
+          pyright
+          shellcheck
+          qemu
+          cava
+        ];
+      };
     };
-  in {
-    packages.${system}.default = pkgs.buildEnv {
-      name = "shyweedsTools";
-
-      paths = with pkgs; [
-        shellharden
-        ruff
-        nixfmt
-        nixd
-        black
-        stylua
-        pyright
-        shellcheck
-        qemu
-        cava
-      ];
-    };
-  };
 }
